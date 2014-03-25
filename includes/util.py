d9256c220c5a1779e60ecb52af87d438107ee5f5
Miner <miner@mining-rig.(none)>
2014-03-24 22:26:11 -0400

    updated minecontrol with logging; merged healthchecks and utils

--- includes/util.py
+++ includes/util.py
@@ -1,8 +1,70 @@
+import socket
+import sys
+import json
+import urllib2
+import tempfile
+import os
 import psutil
 
-# kill any cudaminer processes that are currently running
+
+# kill any cudaminer processes that are currently running matching the cmdline arguments given
 def killCuda(instance):
         killed = False
+        PROCNAME = "cudaminer"
+        for proc in psutil.process_iter():
+                try:
+                        #pinfo = proc.as_dict(attrs=['pid', 'name'])
+                        pinfo = proc.as_dict()
+                except psutil.NoSuchProcess:
+                        pass
+                else:
+                        if pinfo['name'] == PROCNAME:
+                                for obj in pinfo['cmdline']:
+                                        if instance in obj:
+                                                p = psutil.Process(pinfo['pid'])
+                                                p.kill()
+                                                killed = True
+        return killed
+
+
+# check stratum connections
+def checkStratum(address,port,worker,password):
+        response = []
+	MESSAGE = "{\"id\": 1, \"method\": \"mining.subscribe\", \"params\": [\"healthcheck\"]}\n"
+	TCP_IP = address
+	TCP_PORT = int(port)
+	BUFFER_SIZE = 1024 
+	try:
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.connect((TCP_IP, TCP_PORT))
+	except Exception, e:
+		print e
+		return False
+	
+	s.send(MESSAGE)
+	data = s.recv(BUFFER_SIZE)
+	for line in data.splitlines():
+		response.append(line)
+	s.close()
+	
+	try:
+		with tempfile.NamedTemporaryFile() as temp:
+			temp.write(response[0])
+			temp.flush()
+			resp = json.load(open(temp.name))
+			ans = resp.get('error')
+			if not ans:
+				temp.close()
+				return True
+			else:
+				temp.close()
+				return False
+	except Exception, e:
+		print e
+		return False
+
+# Check if cudaminer is running
+def checkCuda(instance):
         PROCNAME = "cudaminer"
         for proc in psutil.process_iter():
                 try:
@@ -11,10 +73,8 @@ def killCuda(instance):
                 except psutil.NoSuchProcess:
                         pass
                 else:
-                        if pinfo['name'] == PROCNAME:
-				for obj in pinfo['cmdline']:
-					if instance in obj:
-						p = psutil.Process(pinfo['pid'])
-                                		#p.kill()
-						killed = True
-        return killed
+        		if pinfo['name'] == PROCNAME:
+                                for obj in pinfo['cmdline']:
+                                        if instance in obj:
+                                        	return True
+	return False
--- minecontrol.py
+++ minecontrol.py
@@ -14,50 +14,44 @@ import Queue
 import subprocess
 import psutil
 import time
-from includes.healthchecks import *
+import logging
+#from includes.healthchecks import *
+from includes.util import *
 from time import localtime, strftime
 from ConfigParser import SafeConfigParser
 
-# kill any cudaminer processes that are currently running
-def killCuda():
-	killed = False
-	PROCNAME = "cudaminer"
-	for proc in psutil.process_iter():
-		try:
-        		pinfo = proc.as_dict(attrs=['pid', 'name'])
-    		except psutil.NoSuchProcess:
-        		pass
-    		else:
-        		if pinfo['name'] == PROCNAME:
-				p = psutil.Process(pinfo['pid'])
-				p.kill()
-				killed = True
-	return killed	
-	
 
 # Main function
 def main(config,pool):
-	outfile = open('miner.log','a')
-	outfile.write("\n[*] Running minecontrol.py at %s\n" % (strftime("%a, %d %b %Y %H:%M:%S +0000", localtime())))
+	logger = logging.getLogger(__name__)
+	logger.setLevel(logging.INFO)
+	outfile = '/home/miner/mining/minecontrol/miner.log'
+	handler = logging.FileHandler(outfile)
+	handler.setLevel(logging.INFO)
+	formatter = logging.Formatter('%(message)s')
+	handler.setFormatter(formatter)
+	logger.addHandler(handler)
+	logger.info("---------------- Starting Minecontrol ----------------")
+	logger.info("[*] Running minecontrol.py at %s" % (strftime("%a, %d %b %Y %H:%M:%S +0000", localtime())))
 	
 	# kill all cuda processes running before continuing
-	outfile.write("[*] Killing cudaminer processes: %s\n" % str(killCuda()))
+	logger.info("[*] Killing cudaminer processes: %s" % str(killCuda('')))
 	
 	if pool:
-		print "[D] Manual pool set: " + pool
+		logger.debug("[D] Manual pool set: %s" % pool)
 	else:
-		print "[D] Using config to pull pool data: " + config
+		logger.debug("[D] Using config to pull pool data: %s" % config)
 		cp = SafeConfigParser()
         	cp.optionxform = str # Preserves case sensitivity
         	cp.readfp(open(config, 'r'))
 		main_section = 'Main'
                 gpu_split = int(cp.get(main_section,'gpu_split'))
                 if gpu_split == 0:
-                        print "[D] Hash split = 100% (or ZERO split)"
+                        logger.info("[*] Hash split = 100% (or ZERO split)")
                 elif gpu_split == 1:
-                        print "[D] Hash split = 50/50"
+                        logger.info("[*] Hash split = 50/50")
                 else:
-                        print "[D] Hash split = unknown (using 100%)"
+                        logger.info("[*] Hash split = unknown (using 100%)")
         	
 		# Primary pool config
 		primary_section = 'Primary'
@@ -89,34 +83,54 @@ def main(config,pool):
 		# Start the cudaminer process
 		if gpu_split == 0:
 			while True:
-				if not checkCuda():
-					outfile.write(" [*] Cudaminer starting at ZERO split\n")
-					subprocess.Popen(["cudaminer", "-S", "-o", primary_connection,"-u", primary_worker, "-p", pri_worker_pw], stdout=subprocess.PIPE)
-				else:
-					if not checkStratum(primary_pool,primary,port,primary_worker,pri_worker_pw):
-						outfile.write(" [!] Primary connection down. Connecting to secondary...")
-						subprocess.Popen(["cudaminer", "-S", "-o", secondary_connection,"-u", secondary_worker, "-p", sec_worker_pw], stdout=subprocess.PIPE)
+				if checkStratum(primary_pool,primary_port,primary_worker,pri_worker_pw):
+					if not checkCuda(primary_connection):
+						if checkCuda(secondary_connection):
+							logger.info("[*] Secondary pool connected while primary is up. Killing secondary")
+							killCuda(secondary_connection)
+						logger.info("[*] Connecting to primary pool at %s" % primary_connection)
+						subprocess.Popen(["cudaminer", "-S", "-o", primary_connection,"-u", primary_worker, "-p", pri_worker_pw], stdout=subprocess.PIPE)
 					else:
 						time.sleep(10)
+				else:
+					logger.info(" [!] Primary pool down!")
+					if checkCuda(primary_connection):
+						logger.info("[!] Killing primary connection...")
+						killCuda(primary_connection)
+					if checkStratum(secondary_pool,secondary_port,secondary_worker,sec_worker_pw):
+						if not checkCuda(secondary_connection):
+							logger.info("[!] Connecting to secondary pool at %s..." % secondary_connection)
+							subprocess.Popen(["cudaminer", "-S", "-o", secondary_connection,"-u", secondary_worker, "-p", sec_worker_pw], stdout=subprocess.PIPE)
+						else:
+							time.sleep(10)
+					else:
+						logger.info("[!] Secondary pool down!!")
 		elif gpu_split == 1:
 			while True:
-				if not checkCuda():
-					outfile.write(" [*] Cudaminer starting at 50/50 split\n")
+				if not checkCuda(primary_connection):
+					logger.info("[*] Cudaminer starting for %s" % primary_connection)
 					subprocess.Popen(["cudaminer", "-S", "-d0", "-o", primary_connection,"-u", primary_worker, "-p", pri_worker_pw], stdout=subprocess.PIPE)
-					subprocess.Popen(["cudaminer", "-S", "-d1", "-o", secondary_connection,"-u", secondary_worker, "-p", sec_worker_pw], stdout=subprocess.PIPE)			
+					if not checkCuda(secondary_connection):	
+						logger.info("[*] Cudaminer starting for %s" % secondary_connection)
+						subprocess.Popen(["cudaminer", "-S", "-d1", "-o", secondary_connection,"-u", secondary_worker, "-p", sec_worker_pw], stdout=subprocess.PIPE)			
+					time.sleep(10)	
 				else:
+					if not checkCuda(secondary_connection):	
+						logger.info("[*] Cudaminer starting for %s" % secondary_connection)
+						subprocess.Popen(["cudaminer", "-S", "-d1", "-o", secondary_connection,"-u", secondary_worker, "-p", sec_worker_pw], stdout=subprocess.PIPE)			
+					
 					#if not checkStratum(primary_pool,primary_port,primary_worker,pri_worker_pw):
-                                        #	outfile.write(" [!] Primary connection down. Connecting to tertiary...")
+                                        #	logger.info(" [!] Primary connection down. Connecting to tertiary...")
                                         #        subprocess.Popen(["cudaminer", "-S", "-d0","-o", tert_connection,"-u", tert_worker, "-p", tert_worker_pw], stdout=subprocess.PIPE)
 					#else:
                                         #        time.sleep(5)
 					#if not checkStratum(secondary_pool,secondary_port,secondary_worker,sec_worker_pw):
-					#	outfile.write(" [!] Secondary connection down. Connecting to tertiary...")
+					#	logger.info(" [!] Secondary connection down. Connecting to tertiary...")
                                         #        subprocess.Popen(["cudaminer", "-S", "-d1","-o", tert_connection,"-u", tert_worker, "-p", tert_worker_pw], stdout=subprocess.PIPE)                                   
 					#else:
 					time.sleep(10)
 		else: 
-			outfile.write(" [X] No split found. Cudaminer not started\n")
+			logger.error("[X] No split found. Cudaminer not started\n")
 
 # Start here
 if __name__ == "__main__":
